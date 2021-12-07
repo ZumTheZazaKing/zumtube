87c56a4a92b7caef588afca22cb90615d29b3f1f
ZumTheZazaKing <zahidi85543@gmail.com>
2021-12-07 14:29:38 +0800

    Can now upload and store in database

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
 import { Context } from '../context/Context';
-import { lazy, Suspense } from 'react'; 
+import { lazy, Suspense, useEffect } from 'react'; 
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import { onSnapshot, doc, setDoc } from 'firebase/firestore';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Topbar = lazy(() => import('./Topbar').then(module => ({default:module.Topbar})));
 const Home = lazy(() => import('./Home').then(module => ({default:module.Home})));
@@ -11,6 +14,28 @@ const Create = lazy(() => import('./Create').then(module => ({default:module.Cre
 function App() {
 
   const [user] = useAuthState(auth);
+
+  useEffect(
+    () => {
+      if(!user)return;
+
+      onSnapshot(doc(db,"users",auth.currentUser.uid), snapshot => {
+        if(!snapshot.exists()){
+            handleNewUser();
+        }
+      })
+    },[user]
+  )
+
+  const handleNewUser = async () => {
+    const userDocRef = doc(db,"users",auth.currentUser.uid);
+    const userPayload = {
+      name:auth.currentUser.displayName,
+      avatar:auth.currentUser.photoURL,
+      description:"No description yet"
+    }
+    await setDoc(userDocRef, userPayload);
+  }
 
   return (
     <HashRouter>
@@ -26,6 +51,18 @@ function App() {
               <Route exact path="/create" element={<Create/>}/>
 
             </Routes>
+            <ToastContainer
+              position="bottom-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="dark"
+            />
           </Context.Provider>
         </Suspense>
       </div>
--- src/components/Create.js
+++ src/components/Create.js
@@ -2,8 +2,10 @@ import { useContext, useEffect, useState } from 'react';
 import { Context } from '../context/Context';
 import { useNavigate } from 'react-router-dom';
 import { auth, db, storage } from '../firebase';
-import { ref, uploadBytes, getDownloadURL } from '@firebase/storage';
+import { ref, uploadBytes, getDownloadURL, uploadBytesResumable } from '@firebase/storage';
 import { addDoc, collection, serverTimestamp } from '@firebase/firestore';
+import { toast } from 'react-toastify';
+import LinearProgress from '@mui/material/LinearProgress';
 import '../styles/Create.css';
 
 export const Create = () => {
@@ -22,10 +24,28 @@ export const Create = () => {
         thumbnail:null
     });
 
+    const [uploadProgress, setUploadProgress] = useState(null);
+
     const onSubmit = async e => {
         e.preventDefault();
-        const currentDate = new Date().toLocaleString();
-        console.log("WRYYY");
+
+        if(!videoData.title)return toast.warning("Please insert a title");
+        if(!videoData.video)return toast.warning("Please upload a video");
+        if(!videoData.thumbnail)return toast.warning("Please upload a thumbnail")
+
+        const currentDate = new Date().toLocaleDateString();
+        await addDoc(collection(db,'videos'),{
+            title:videoData.title,
+            description:videoData.description,
+            video:videoData.video,
+            thumbnail:videoData.thumbnail,
+            author:auth.currentUser.uid,
+            createdAt:serverTimestamp(),
+            date:currentDate
+        })
+        .then(() => toast.success("Video created"))
+        .catch(() => toast.error("Something went wrong"))
+
         navigate("/")
     }
 
@@ -33,28 +53,33 @@ export const Create = () => {
 
         if(!e.target.files[0])return;
 
-        console.log(e.target.files[0]);
-
         const storageRef = ref(storage,`/${auth.currentUser.uid}Images/${e.target.files[0].name}`);
 
         await uploadBytes(storageRef, e.target.files[0]);
         await getDownloadURL(ref(storage,`/${auth.currentUser.uid}Images/${e.target.files[0].name}`))
-        .then(url => {setVideoData({...videoData, thumbnail:url}); console.log(url)});
-
+        .then(url => {setVideoData({...videoData, thumbnail:url});});
     }
 
     const videoUpload = async(e) => {
 
         if(!e.target.files[0])return;
 
-        console.log(e.target.files[0]);
-
         const storageRef = ref(storage,`/${auth.currentUser.uid}Videos/${e.target.files[0].name}`);
 
-        await uploadBytes(storageRef, e.target.files[0]);
-        await getDownloadURL(ref(storage,`/${auth.currentUser.uid}Videos/${e.target.files[0].name}`))
-        .then(url => {setVideoData({...videoData, video:url}); console.log(url)});
+        const uploadTask = uploadBytesResumable(storageRef, e.target.files[0]);
 
+        uploadTask.on("state_changed", (snapshot) => {
+            const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+            setUploadProgress(progress);
+
+        },(err) => {
+            toast.error("Video failed to upload")
+
+        }, () => {
+            getDownloadURL(uploadTask.snapshot.ref).then(url => {
+                setVideoData({...videoData, video:url})
+            })
+        })
     }
 
     return (<div id="create">
@@ -63,20 +88,25 @@ export const Create = () => {
             <h3>Title</h3>
             <input type="text" 
             value={videoData.title} 
-            onChange={e => setVideoData({...videoData, title:e.target.value})} 
-            required/>
+            onChange={e => setVideoData({...videoData, title:e.target.value})}/>
             <br/><br/>
 
             <h3>Description</h3>
             <textarea 
             value={videoData.description} 
-            onChange={e => setVideoData({...videoData, description:e.target.value})} 
-            required></textarea>
+            onChange={e => setVideoData({...videoData, description:e.target.value})}></textarea>
             <br/><br/>
 
             <h2>Video Upload</h2>
             <input type="file" accept="video/*" 
-            onChange={e => videoUpload(e)}required/>
+            onChange={e => videoUpload(e)}/>
+            
+            {uploadProgress ? 
+            <div id="videoUploadProgress">
+                <br/><br/>
+                <LinearProgress id="videoUploadProgressBar" variant="determinate" value={uploadProgress}/>
+                <p id="videoUploadPercentage">{uploadProgress}%</p>
+            </div> : ""}
             <br/>
             <video id="videoPreview" src={videoData.video} controls>
                 Your browser does not support the video format
@@ -85,7 +115,7 @@ export const Create = () => {
 
             <h2>Thumbnail Upload</h2>
             <input type="file" accept="image/*" 
-            onChange={(e) => imageUpload(e)} required/>
+            onChange={(e) => imageUpload(e)}/>
             <br/>
             <img alt="" id="thumbPreview" src={videoData.thumbnail}/>
             <br/><br/>
