c7644ca54a9c5b8409dbe2c8296d941d2a58c86f
ZumTheZazaKing <zahidi85543@gmail.com>
2021-12-11 16:48:01 +0800

    Cannow go to channel through comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -4,13 +4,16 @@ import moment from 'moment';
 import { auth } from '../firebase';
 import { useContext } from 'react';
 import { Context } from '../context/Context';
+import { useNavigate } from 'react-router-dom';
 
 export const Comment = (props) => {
 
     const { avatar, createdAt, comment, name, id, commentId } = props.info;
     const { user } = useContext(Context);
+    const navigate = useNavigate();
 
     return (<div className="comment" id={commentId}
+    onClick={() => {navigate(`/channel/${id}`)}}
     onContextMenu={user ? (auth.currentUser.uid === id ? props.onContextMenu : () => {return}) : () => {return}}>
         <Avatar src={avatar} alt={name}/>
         <div id="commentBody">
